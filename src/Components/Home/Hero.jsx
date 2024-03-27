6e16064c3ecde3be1427e1ddeb7cc8f29dd62bb3
Manoj Kumar <manojkumararumainathan@gmail.com>
2024-03-27 14:37:48 +0530

    Nav and Hero

---
+++ src/Components/Home/Hero.jsx
@@ -0,0 +1,17 @@
+import Logo from '../../assets/logo.png'
+
+const Hero = () => {
+  return (
+    <div>
+        <div>
+            <div>
+                <div>
+                    <img src={Logo} alt="" />
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Hero
--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -3,15 +3,17 @@ import Logo from '../../assets/logo.png'
 const NavBar = () => {
   return (
     <div>
-        <div className=" p-8">
-            <div className=" flex justify-around">
-                <p className=' cursor-pointer text-gray-700 font-medium'>Home</p>
-                <p className=' cursor-pointer text-gray-700 font-medium'>About</p>
-                <img src={Logo} width={80}/>
-                <p className=' cursor-pointer text-gray-700 font-medium'>Events</p>
-                <p className=' cursor-pointer text-gray-700 font-medium'>Contact</p>
-            </div>
+      <div className=" p-8 flex justify-between px-14">
+        <div>
+          <img src={Logo} width={80} />
         </div>
+        <div className=" flex gap-[120px]">
+          <p className=' cursor-pointer text-gray-700 font-medium'>Home</p>
+          <p className=' cursor-pointer text-gray-700 font-medium'>About</p>
+          <p className=' cursor-pointer text-gray-700 font-medium'>Events</p>
+          <p className=' cursor-pointer text-gray-700 font-medium'>Contact</p>
+        </div>
+      </div>
     </div>
   )
 }
