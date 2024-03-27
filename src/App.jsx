7e3e265642e899b561401457ba6c06e26c43921b
BALASANTHOSH01 <santhos01ac@gmail.com>
2024-03-27 15:29:32 +0530

    hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
+import Hero from "./Components/Home/Hero";
 import NavBar from "./Components/NavBar/NavBar"
 
 const App = () => {
   return (
     <div>
       <NavBar/>
+      <Hero/>
     </div>
   )
 }
 
-export default App
+export default App;
--- src/Components/Home/Hero.jsx
+++ src/Components/Home/Hero.jsx
@@ -1,8 +1,26 @@
-
+import InfoLogo from "../../assets/logo.png";
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <div>
+        <div>
+            <div className="mx-auto my-[3%]">
+                <img src={InfoLogo} alt="Infest_2k24" className="mx-auto w-[30%]"/>
+                <h1 className="text-center my-[1.5%] text-[25px] text-green-500 font-bold">23rd April</h1>
+
+                <div className=" flex flex-row justify-evenly items-center mx-auto text-center my-[2%] w-[38%] font-semibold">
+                    <div className=" text-[20px] hover:shadow-lg px-[50px] py-[15px] border-[2px] rounded-[8px] text-white bg-blue-500 border-blue-500 cursor-pointer">
+                        <p>Register&#160;Events</p>
+                    </div>
+
+                    <div className=" text-[20px] hover:shadow-lg px-[50px] py-[15px] border-[2px] rounded-[8px] border-green-500 cursor-pointer">
+                        <p>Contact&#160;Us</p>
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+    </div>
   )
 }
 
