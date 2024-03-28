3d1aa2be49f6a9d42519ab1cf103711a98929e55
Manoj Kumar <manojkumararumainathan@gmail.com>
2024-03-28 18:53:46 +0530

    Color

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,8 +1,8 @@
 import { useEffect, useState } from 'react';
-import Logo from '../../assets/logo.png'
-
+import Logo from '../../assets/logo.png';
 import { RiMenu4Line as Menu } from "react-icons/ri";
 import MenuItems from './MenuItems';
+import { Link } from 'react-router-dom';
 
 
 const NavBar = () => {
@@ -14,7 +14,7 @@ const NavBar = () => {
   return (
     <div>
       <div className=" flex justify-between p-8 px-20 sticky">
-        <img src={Logo} width={80} />
+        <Link to={'/'} className=' cursor-pointer'><img src={Logo} width={80} /></Link>
         <div className=' text-[24px] cursor-pointer duration-500 ease-in'>
           {
             ShowMenu===false ? <Menu className='text-[32px]' onClick={()=>setShowMenu((prev)=>!prev)}/> : <MenuItems setShowMenu={setShowMenu} />
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import Hero from "../../Components/Home/Hero.jsx"
 import HomeDept from "../../Components/Home/HomeDept/HomeDept.jsx";
+import { Bento, Box } from '@primer/react-brand'
 
 const Home = () => {
   return (
     <div>
       <div>
-        <Hero/>
-        <HomeDept/>
+        <Hero />
+        <HomeDept />
+        
       </div>
     </div>
   )
