f580acf9120163bfdda396923265be6fefd7ff5d
Manoj Kumar <manojkumararumainathan@gmail.com>
2024-03-30 22:43:25 +0530

    mobile view- about

--- src/Components/Home/About.jsx
+++ src/Components/Home/About.jsx
@@ -35,7 +35,7 @@ const About = () => {
               <div className=" bg-white p-1 px-2 m-2 w-auto rounded-md font-bold shadow-xl border-gray-200 border">
                 <p className=" flex items-center gap-2 md:text-[14px]"><Event className=" text-blue-500"/>25+ Events on Infest 2K24</p>
               </div>
-              <div className=" bg-white p-1 px-2 m-2 w-[70%] rounded-md font-bold  shadow-xl border-gray-200 border">
+              <div className=" bg-white p-1 px-2 m-2 w-[75%] rounded-md font-bold  shadow-xl border-gray-200 border">
                 <p className=" flex items-center gap-2 md:text-[14px]"><Prize className=" text-yellow-600"/>Exiciting Prizes</p>
               </div>
             </div>
