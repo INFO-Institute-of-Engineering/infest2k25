a4be2b86e0bcb7178a288b87b5fe228176962aef
BALASANTHOSH01 <santhos01ac@gmail.com>
2024-04-02 22:55:58 +0530

    event page updation

--- src/Components/Events/Event.jsx
+++ src/Components/Events/Event.jsx
@@ -1,8 +1,9 @@
 import { useEffect, useState } from "react";
 import { Link, Outlet, useLocation } from "react-router-dom";
+import { Wave1 } from "../TextAnimation/TextAnimation.jsx";
 
 const Event = () => {
-  document.documentElement.scrollTop=0;
+  document.documentElement.scrollTop = 0;
   const location = useLocation();
   const [showEvent, setShowEvent] = useState("tech");
 
@@ -11,7 +12,7 @@ const Event = () => {
     const pathname = location.pathname;
     const eventCategory = pathname.split("/")[2];
     setShowEvent(eventCategory || "tech");
-    
+
     // Scroll to the top on component mount or update
     document.documentElement.scrollTop = 0;
   }, [location.pathname]);
@@ -20,7 +21,10 @@ const Event = () => {
   const renderLink = (category, label) => (
     <Link
       to={`/event/${category}`}
-      className={`cursor-pointer ${showEvent === category && "bg-gradient-to-r from-green-500 to-blue-500 text-white"} duration-700 ease-linear transition px-[2%] py-[1%] rounded-[25px] text-[15px]`}
+      className={`cursor-pointer ${
+        showEvent === category &&
+        "bg-gradient-to-r from-green-500 to-blue-500 text-white"
+      } duration-700 ease-linear transition px-[2%] py-[1%] rounded-[25px] text-[15px]`}
       onClick={() => setShowEvent(category)}
     >
       {label}
@@ -30,7 +34,7 @@ const Event = () => {
   return (
     <div className="mt-[10%] mb-[5%] sm:mt-[23%]">
       <div className="my-4 text-[60px] font-extrabold text-center [-webkit-text-stroke:1px_#0092ff] text-transparent animate-colorChange">
-        E V E N T S
+        <Wave1 /> {/** Event animated text */}
       </div>
 
       <div className="font-medium text-center">
