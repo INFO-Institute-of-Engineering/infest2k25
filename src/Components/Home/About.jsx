54781face08174a9678958162086fe1b8698a67b
Alphin V T <alphoht@gmail.com>
2025-03-11 12:12:24 +0530

    added content in about us

--- src/Components/Home/About.jsx
+++ src/Components/Home/About.jsx
@@ -11,22 +11,14 @@ const About = () => {
           <div className=" w-[50%] md:w-[90%] gap-10 md:gap-5 flex flex-col">
             <h1 className=" text-[30px] font-bold bg-gradient-to-r from-blue-500 via-green-500 to-green-500 bg-clip-text text-transparent">About Infest</h1>
 
-            <p className=" text-gray-700">Welcome to <strong>Infest 2K24</strong> , the National Level Technical Symposium
-              hosted by Info Institute of Engineering! We're excited to provide a
-              platform for aspiring engineers and tech enthusiasts from all
-              disciplines to showcase their talent and compete for amazing prizes.
+            <p className=" text-gray-700">Welcome to <strong>INFEST 2K25</strong>, the National Level Technical Symposium organized by <strong>INFO Institute of Engineering!</strong> We're thrilled to offer a platform where aspiring engineers and tech enthusiasts from various disciplines can showcase their skills and compete for exciting prizes.
             </p>
 
-            <p className=" text-gray-700">Calling all students from <strong> CSE, IT, AIDS, ECE, EEE, MECH, S&H, and
-              MBA! </strong>Are you passionate about technology? Do you have groundbreaking
-              research or innovative projects waiting to be unveiled? Infest 2024
-              offers you the perfect opportunity to:
+            <p className=" text-gray-700">Attention students from <strong>CSE, IT, AIDS, ECE, EEE, MECH, S&H, and MBA!</strong>Are you passionate about technology? Do you have groundbreaking research or innovative projects ready to be showcased? <strong>NFEST 2K25</strong> is your ideal platform to:
             </p>
 
             <p className=" text-gray-700">
-              Infest 2K24 is more than just a competition; it's a vibrant community
-              of tech enthusiasts coming together to learn, share, and innovate.
-              Join us for an experience that will never fades away.
+            <strong>INFEST 2K25</strong> is more than just a competition—it's a dynamic community of tech enthusiasts uniting to learn, share, and innovate. Be part of an unforgettable experience that leaves a lasting impact.
             </p>
             
           </div>
