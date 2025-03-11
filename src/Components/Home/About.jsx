8bb1661a63812a9263304066d9a1f7af6bc7f9ab
Alphin V T <alphoht@gmail.com>
2025-03-11 12:26:48 +0530

    added date

--- src/Components/Home/About.jsx
+++ src/Components/Home/About.jsx
@@ -9,7 +9,7 @@ const About = () => {
         <div className=" px-8 pt-8 flex md:flex-col justify-evenly gap-10 items-center">
 
           <div className=" w-[50%] md:w-[90%] gap-10 md:gap-5 flex flex-col">
-            <h1 className=" text-[30px] font-bold bg-gradient-to-r from-blue-500 via-green-500 to-green-500 bg-clip-text text-transparent">About Infest</h1>
+            <h1 className=" text-[30px] font-bold bg-gradient-to-r from-blue-500 via-green-500 to-green-500 bg-clip-text text-transparent">About INFEST</h1>
 
             <p className=" text-gray-700">Welcome to <strong>INFEST 2K25</strong>, the National Level Technical Symposium organized by <strong>INFO Institute of Engineering!</strong> We're thrilled to offer a platform where aspiring engineers and tech enthusiasts from various disciplines can showcase their skills and compete for exciting prizes.
             </p>
@@ -18,7 +18,7 @@ const About = () => {
             </p>
 
             <p className=" text-gray-700">
-            <strong>INFEST 2K25</strong> is more than just a competition—it's a dynamic community of tech enthusiasts uniting to learn, share, and innovate. Be part of an unforgettable experience that leaves a lasting impact.
+            <strong>INFEST 2K25</strong> is more than just a competition, it's a dynamic community of tech enthusiasts uniting to learn, share, and innovate. Be part of an unforgettable experience that leaves a lasting impact.
             </p>
             
           </div>
@@ -29,7 +29,7 @@ const About = () => {
 
             <div className=" absolute top-[60%] right-[50%] w-[60%] md:w-[70%] md:right-[40%]">
               <div className=" bg-white p-1 px-2 m-2 w-auto rounded-md font-bold shadow-xl border-gray-200 border">
-                <p className=" flex items-center gap-2 md:text-[14px]"><Event className=" text-blue-500"/>25+ Events on Infest 2K24</p>
+                <p className=" flex items-center gap-2 md:text-[14px]"><Event className=" text-blue-500"/>25+ Events on INFEST 2K25</p>
               </div>
               <div className=" bg-white p-1 px-2 m-2 w-[75%] rounded-md font-bold  shadow-xl border-gray-200 border">
                 <p className=" flex items-center gap-2 md:text-[14px]"><Prize className=" text-yellow-600"/>Exiciting Prizes</p>
